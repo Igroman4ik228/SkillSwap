b702037a8eb2176bbff69f233c99aa19e3bf3954
Igroman4ik-pc <ni4000009@gmail.com>
2025-10-16 14:42:01 +0300

    refactor: add check types with tsc

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { router, store } from '@/app';
 import '@/app/styles/index.scss';
+import { ModalProvider } from '@/shared';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { ModalProvider } from '@/shared';
 
-const container = document.getElementById('root'!) as HTMLElement;
+const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
